345c8148c78d717c7d36bff65b2c9e8fd7d3557a
wode490390 <wode490390@qq.com>
2019-08-13 13:02:50 +0800

    1.0.1

--- src/main/java/cn/wode490390/nukkit/biomefixer/FixBiomeCommand.java
+++ src/main/java/cn/wode490390/nukkit/biomefixer/FixBiomeCommand.java
@@ -4,8 +4,10 @@ import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.command.PluginIdentifiableCommand;
 import cn.nukkit.lang.TranslationContainer;
+import cn.nukkit.level.Level;
 import cn.nukkit.level.Position;
 import cn.nukkit.level.biome.BiomeSelector;
+import cn.nukkit.level.biome.EnumBiome;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.math.NukkitRandom;
 import cn.nukkit.plugin.Plugin;
@@ -27,14 +29,32 @@ public class FixBiomeCommand extends Command implements PluginIdentifiableComman
             return false;
         }
         if (sender.isPlayer()) {
-            FullChunk chunk = ((Position) sender).getChunk();
-            BiomeSelector selector = new BiomeSelector(new NukkitRandom(chunk.getProvider().getSeed()));
-            int x = chunk.getX() << 4;
-            int z = chunk.getZ() << 4;
-            for (int cx = 0; cx < 16; cx++) {
-                for (int cz = 0; cz < 16; cz++) {
-                    chunk.setBiome(cx, cz, selector.pickBiome(x | cx, z | cz));
-                }
+            Position position = (Position) sender;
+            FullChunk chunk = position.getChunk();
+            switch (position.getLevel().getDimension()) {
+                case Level.DIMENSION_NETHER:
+                    for (int cx = 0; cx < 16; cx++) {
+                        for (int cz = 0; cz < 16; cz++) {
+                            chunk.setBiome(cx, cz, EnumBiome.HELL.biome);
+                        }
+                    }
+                    break;
+                case Level.DIMENSION_THE_END:
+                    for (int cx = 0; cx < 16; cx++) {
+                        for (int cz = 0; cz < 16; cz++) {
+                            chunk.setBiomeId(cx, cz, 9); //EnumBiome.THE_END.biome
+                        }
+                    }
+                    break;
+                default:
+                    BiomeSelector selector = new BiomeSelector(new NukkitRandom(chunk.getProvider().getSeed()));
+                    int x = chunk.getX() << 4;
+                    int z = chunk.getZ() << 4;
+                    for (int cx = 0; cx < 16; cx++) {
+                        for (int cz = 0; cz < 16; cz++) {
+                            chunk.setBiome(cx, cz, selector.pickBiome(x | cx, z | cz));
+                        }
+                    }
             }
             sender.sendMessage("The biome in the current chunk has been successfully fixed.");
         } else {
